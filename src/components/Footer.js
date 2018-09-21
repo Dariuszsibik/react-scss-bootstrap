2457f1affdd45ffbda0e13557e7dc225a07c2297
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-09-21 15:34:38 +0200

    add styles Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,26 +1,48 @@
 import React, { Component } from 'react';
+import FooterIconEmail from '../images/ui/contact-footer-icon-email.png';
+import FooterIconPhone from '../images/ui/contact-footer-icon-phone.png';
+import FooterIconPin from '../images/ui/contact-footer-icon-pin.png';
+
 
 class Footer extends Component {
     render() {
         return (
-<div className="ds-footer-top">
-    <div className="container ds-footer-top-content">
-        <div className="row">
-            <div className="col-md-6 col-lg-3m mb-5">
-                <h4 class="text-uppercase text-white">O nas</h4>
-            </div>
-            <div className="col-md-6 col-lg-3m mb-5">
-                <h4 class="text-uppercase text-white">Nawigacja</h4>
-            </div>
-            <div className="col-md-6 col-lg-3m mb-5">
-                <h4 class="text-uppercase text-white">Kontakt</h4>
-            </div>
-            <div className="col-md-6 col-lg-3m mb-5">
-                <h4 class="text-uppercase text-white">Newsletter</h4>
-            </div>
-        </div>
-    </div>
-</div>
+                <div className="ds-footer-top">
+                    <div className="container ds-footer-top-content">
+                        <div className="row">
+                            <div className="col-md-6 col-lg-3 mb-5">
+                                <h4 className="text-uppercase text-white">O nas</h4>
+                                <p className="ds-footer-text ds-footer-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis voluptatum, ratione ducimus sunt voluptates rem perspiciatis tempora sint minus provident, ipsum voluptas esse, fugiat autem temporibus vitae eum nobis! Necessitatibus.</p>
+                            </div>
+                            <div className="col-md-6 col-lg-3 mb-5">
+                                <h4 className="text-uppercase text-white">Nawigacja</h4>
+                                <ul className="list-unstyled">
+                                            <li><a className="ds-footer-text" href="/">Regulamin serwisu</a></li>
+                                            <li><a className="ds-footer-text" href="/">Logowanie</a></li>
+                                            <li><a className="ds-footer-text" href="/">Wsparcie serwisu</a></li>
+                                            <li><a className="ds-footer-text" href="/">FAQ & POMOC</a></li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 col-lg-3 mb-5">
+                                <h4 className="text-uppercase text-white">Kontakt</h4>
+                                <ul className="list-unstyled ds-footer-text ds-footer-contact-list">
+                                    <li className="ds-footer-contact-item">
+                                        <span className="mr-2"><img src={FooterIconPin} alt="Adres" /> Kraków </span>
+                                    </li>
+                                    <li className="ds-footer-contact-item">
+                                        <span className="mr-2"><img src={FooterIconPhone} alt="Adres" /> 883-739-377 </span>
+                                    </li>
+                                    <li className="ds-footer-contact-item">
+                                        <span className="mr-2"><img src={FooterIconEmail} alt="Adres" /> sibikdariusz@gmail.com </span>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="col-md-6 col-lg-3 mb-5">
+                                <h4 className="text-uppercase text-white">Newsletter</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
         )
     }
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,24 +5,24 @@ class Header extends Component {
         return (
                 <div className="bg-ds-dark ds-header">
                     <div className="container">
-                        <ul class="nav flex-column flex-md-row justify-content-lg-end text-uppercase">
-                            <li class="nav-item ds-header-item-active">
-                                <a class="nav-link ds-header-item-link" href="/">Strona Główna</a>
+                        <ul className="nav flex-column flex-md-row justify-content-lg-end text-uppercase">
+                            <li className="nav-item ds-header-item-active">
+                                <a className="nav-link ds-header-item-link" href="/">Strona Główna</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white ds-header-item-link" href="/">Realizacje</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-white ds-header-item-link" href="/">Realizacje</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white ds-header-item-link" href="/">Usługa</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-white ds-header-item-link" href="/">Usługa</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white ds-header-item-link" href="/">Aktualności</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-white ds-header-item-link" href="/">Aktualności</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white ds-header-item-link" href="/">Sklep</a>
+                            <li className="nav-item">
+                                <a className="nav-link text-white ds-header-item-link" href="/">Sklep</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link text-white ds-header-item-link" href="/">O nas
+                            <li className="nav-item">
+                                <a className="nav-link text-white ds-header-item-link" href="/">O nas
                                     <img src="" alt="" />
                                 </a>
                             </li>
