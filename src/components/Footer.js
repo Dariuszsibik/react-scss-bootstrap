bc0f473cffbdbc6d00d6b35df6d70daaa37d8603
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-09-21 14:44:26 +0200

    starting Footer Component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,24 @@ import React, { Component } from 'react';
 class Footer extends Component {
     render() {
         return (
-<div className="ds-footer-top"></div>
+<div className="ds-footer-top">
+    <div className="container ds-footer-top-content">
+        <div className="row">
+            <div className="col-md-6 col-lg-3m mb-5">
+                <h4 class="text-uppercase text-white">O nas</h4>
+            </div>
+            <div className="col-md-6 col-lg-3m mb-5">
+                <h4 class="text-uppercase text-white">Nawigacja</h4>
+            </div>
+            <div className="col-md-6 col-lg-3m mb-5">
+                <h4 class="text-uppercase text-white">Kontakt</h4>
+            </div>
+            <div className="col-md-6 col-lg-3m mb-5">
+                <h4 class="text-uppercase text-white">Newsletter</h4>
+            </div>
+        </div>
+    </div>
+</div>
         )
     }
 }
