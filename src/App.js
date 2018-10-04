31cd35e74025eba7b9b89261caf18f8f88cdcbca
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-10-04 09:53:57 +0200

    delete unused variables

--- src/App.js
+++ src/App.js
@@ -5,51 +5,8 @@ import Hero from './components/Hero.js';
 import Footer from './components/Footer.js';
 
 class App extends Component {
-/*
-  constructor(){
-    super();
-    this.state = {
-      navActive: 'about',
-    };
-    this.onNavClick = this.onNavClick.bind(this);
-  }
-
-    onNavClick(props) {
-      this.setState({
-        navActive: props
-      })
-      if (window.screen.width < 1200) {
-        document.getElementById('navbar-toggler-content').click();
-      }
-    }
-*/
 
   render() {
-/*
-    var section = null;
-    switch (this.state.navActive) {
-      case 'courses':
-          section = <Courses/>;
-          break;
-      case 'about':
-          section = <About/>;
-          break;
-      case 'experience':
-          section = <Experience/>;
-          break;
-      case 'education':
-          section = <Education/>;
-          break;
-      case 'skills':
-          section = <Skills/>;
-          break;
-      case 'projects':
-          section = <Projects/>;
-          break;
-      default:
-          section = null;
-  }
-  */
 
     return (
       <div>
