e0c84c6e929dfd209475805dce056eeba5d119ec
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-10-09 15:25:12 +0200

    Test react-router

--- src/App.js
+++ src/App.js
@@ -5,20 +5,33 @@ import Hero from './components/Hero.js';
 import Footer from './components/Footer.js';
 import Copyright from './components/Copyright';
 import Welcome from './components/Welcome';
+import Education from './components/Education';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 class App extends Component {
 
   render() {
 
     return (
+      <Router>
       <div>
-        <Nav/>
-        <Hero/>
-        <Welcome/>
-        <Footer/>
-        <Copyright/>
+          <Nav/>
+          <Hero/>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/test" component={Education} />
+          <Welcome/>
+          <Footer/>
+          <Copyright/>
+
       </div>
-    );
+
+      </Router>
+      );
   }
 }
 
---
+++ src/components/Education.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const Education = () => (
+    <section className="ds-front-page-welcome">
+        <h2>Education</h2>
+    </section>
+    );
+    export default Education;
