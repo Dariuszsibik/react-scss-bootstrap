b3bc13daa58c6e562e2f169134cdcfa0b755b3dd
Dariusz Sibik <dariusz.sibik@uj.edu.pl>
2018-10-23 16:03:47 +0200

    add styles for Aktualnosci Component

--- src/components/Aktualnosci.js
+++ src/components/Aktualnosci.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Aktualnosci1 from '../images/pages/aktualnosci-front-1.jpg';
+import Aktualnosci2 from '../images/pages/aktualnosci-front-2.jpg';
 import DateIcon from '../images/ui/date-icon.png';
 
 const Aktualnosci = () => (
@@ -14,7 +15,7 @@ const Aktualnosci = () => (
                     <div className="col-lg-6">
                         <div className="ds-post-box">
                             <div className="text-center bg-primary d-flex align-items-center ds-post-box-date">
-                                <div className="w-50 ds-post-box-date-body">
+                                <div className="mx-auto ds-post-box-date-body">
                                     <img className="d-none d-md-block mx-auto" src={DateIcon} alt="Kalendarz"/>
                                     <span className="text-white d-block ds-post-box-date-value">23 WRZ 2018</span>
                                 </div>
@@ -33,6 +34,29 @@ const Aktualnosci = () => (
                             </div>
                         </div>
                     </div>
+                    <div className="col-lg-6">
+                        <div className="ds-post-box">
+                            <div className="text-center bg-primary d-flex align-items-center ds-post-box-date">
+                                <div className="mx-auto ds-post-box-date-body">
+                                    <img className="d-none d-md-block mx-auto" src={DateIcon} alt="Kalendarz"/>
+                                    <span className="text-white d-block ds-post-box-date-value">28 LIS 2018</span>
+                                </div>
+                            </div>
+                            <div className="ds-portfolio-box-body">
+                                <div className="ds-post-box-img">
+                                    <img className="img-fluid" src={Aktualnosci2} alt="post 2"/>
+                                </div>
+                                <div className="text-white ds-post-box-content">
+                                    <div className="ds-post-box-content-inner">
+                                        <h3 className="text-uppercase ds-post-box-title">Lorem, ipsum</h3>
+                                        <p className="ds-post-box-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos modi deleniti molestias beatae voluptate a animi, ipsam, delectus consequatur dolor possimus quaerat iste! Repellendus, provident culpa? Sunt assumenda dolore nihil?</p>
+                                        <a className="text-primary text-uppercase ds-post-box-link" href="/">Zobacz więcej</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </section>
