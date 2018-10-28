9ead3e52d90807d87e44f870c06d3c88556d3079
Dariusz Sibik <sibikdariusz@gmail.com>
2018-10-28 15:39:55 +0100

    add About-content component, and styled about page

---
+++ src/components/About/About-content.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Avatar1 from '../../images/pages/about-avatar-1.png';
+
+const AboutContent = () => (
+
+        <div className="d-flex flex-wrap justify-content-between mb-2">
+            <div className="text-uppercase text-center ds-about-avatar">
+                <img className="ds-about-avatar-img" src={Avatar1} alt="Jan Kowalski" />
+                <span className="d-block">Projektant</span>
+                <span className="d-block text-primary ds-about-avatar-name">Jan Kowalski</span>
+                <span className="d-block">www.adres.pl</span>
+            </div>
+            <div className="text-uppercase text-center ds-about-avatar">
+                <img className="ds-about-avatar-img" src={Avatar1} alt="Jan Kowalski" />
+                <span className="d-block">Projektant</span>
+                <span className="d-block text-primary ds-about-avatar-name">Jan Kowalski</span>
+                <span className="d-block">www.adres.pl</span>
+            </div>
+            <div className="text-uppercase text-center ds-about-avatar">
+                <img className="ds-about-avatar-img" src={Avatar1} alt="Jan Kowalski" />
+                <span className="d-block">Projektant</span>
+                <span className="d-block text-primary ds-about-avatar-name">Jan Kowalski</span>
+                <span className="d-block">www.adres.pl</span>
+            </div>
+            <div className="text-uppercase text-center ds-about-avatar">
+                <img className="ds-about-avatar-img" src={Avatar1} alt="Jan Kowalski" />
+                <span className="d-block">Projektant</span>
+                <span className="d-block text-primary ds-about-avatar-name">Jan Kowalski</span>
+                <span className="d-block">www.adres.pl</span>
+            </div>
+            <div className="text-uppercase text-center ds-about-avatar">
+                <img className="ds-about-avatar-img" src={Avatar1} alt="Jan Kowalski" />
+                <span className="d-block">Projektant</span>
+                <span className="d-block text-primary ds-about-avatar-name">Jan Kowalski</span>
+                <span className="d-block">www.adres.pl</span>
+            </div>
+            <div className="text-uppercase text-center ds-about-avatar">
+                <img className="ds-about-avatar-img" src={Avatar1} alt="Jan Kowalski" />
+                <span className="d-block">Projektant</span>
+                <span className="d-block text-primary ds-about-avatar-name">Jan Kowalski</span>
+                <span className="d-block">www.adres.pl</span>
+            </div>
+        </div>
+
+    );
+    export default AboutContent;
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import AboutImg from '../../images/pages/about-img.png';
+import AboutTop from './About-top.js'
+import AboutContent from './About-content.js'
 
 const About = () => (
 
         <div className="ds-about-page">
             <div className="container">
-                <div className="row">
-                    <div className="col-lg-6">
-                        <span className="text-muted text-uppercase ds-text-small ds-page-intro-subheader">Lorem ipsum dolor,
-                            sit amet consectetur adipisicing elit.</span>
-                        <h2 className="text-uppercase ds-page-intro-header">Poznaj <span className="text-primary">Naszą
-                                Firmę</span></h2>
-                        <div className="ds-hr ds-page-intro-rule"></div>
-                        <p>Sit aliquip ipsum ex Lorem aliquip ullamco aliqua nisi commodo magna. Officia ad ullamco nisi proident cillum aute magna commodo incididunt et. Id cillum ad irure et reprehenderit incididunt sint. Magna cupidatat consectetur ex dolore labore labore nostrud. Ex ea ex consectetur excepteur consectetur Lorem eu esse labore eiusmod aliquip aute. In magna nulla qui Lorem in in. In in occaecat sint consequat aliquip in veniam.
-                        Anim ad in occaecat aliquip velit occaecat non. Fugiat id quis quis ad adipisicing nisi sint id culpa consequat. Enim culpa tempor excepteur nulla et anim. Non incididunt velit voluptate do aute amet. Duis laboris nisi magna minim sit et id do duis eiusmod nulla pariatur est ut. Tempor ex nostrud ea aliquip eu sint elit tempor. Consectetur veniam commodo pariatur eu proident aliqua ex commodo labore voluptate.</p>
-                    </div>
-                    <div className="col-lg-6">
-                        <img className="img-fluid mt-2" src={AboutImg} alt="nasza firma"/>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <p>Ipsum eu qui ad duis non. Commodo irure sit adipisicing officia ut ut dolore commodo aliquip tempor et elit do proident. Officia do irure cillum commodo mollit mollit duis eiusmod adipisicing dolore tempor. Fugiat ullamco laboris nulla occaecat qui ea excepteur aliquip consectetur deserunt pariatur culpa. Elit velit laborum non nostrud culpa.</p>
-                    </div>
-                </div>
+                <AboutTop/>
+                <AboutContent/>
             </div>
         </div>
 
