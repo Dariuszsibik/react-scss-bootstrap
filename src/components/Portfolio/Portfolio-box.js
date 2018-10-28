b767abb4ae3509e9fcf3110ec8366df8674bfe88
Dariusz Sibik <sibikdariusz@gmail.com>
2018-10-28 18:18:23 +0100

    add component Portfolio-box

---
+++ src/components/Portfolio/Portfolio-box.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import Portfolio1 from '../../images/portfolio/portfolio-1.jpg';
+import Portfolio2 from '../../images/portfolio/portfolio-2.jpg';
+import Portfolio3 from '../../images/portfolio/portfolio-3.jpg';
+
+const PortfolioBox = () => (
+
+                <div className="d-flex flex-wrap justify-content-around justify-content-xl-between ds-portfolio">
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio1} alt="Realizacja-1"/>
+                        </div>
+                        <div className="text-center text-white ds-portfolio-box-text">
+                            <h4 className="text-uppercase ds-portfolio-box-header">Najnowszy projekt</h4>
+                            <span className="d-block ds-portfolio-item-info">
+                                <span className="text-uppercase">Klient:</span> XYZ Construction
+                            </span>
+                            <span className="d-block ds-portfolio-item-info">
+                                <span className="text-uppercase">Lokalizacja:</span> Warszawa
+                            </span>
+                            <span className="d-block ds-portfolio-item-info">
+                                <span className="text-uppercase">Powierzchnia:</span> 140m2
+                            </span>
+                            <span className="d-block ds-portfolio-item-info">
+                                <span className="text-uppercase">wartość:</span> 900 000zł
+                            </span>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio2} alt="Realizacja-2"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio2} alt="Realizacja-2"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio1} alt="Realizacja-1"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio2} alt="Realizacja-2"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio2} alt="Realizacja-2"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio1} alt="Realizacja-1"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio2} alt="Realizacja-2"/>
+                        </div>
+                    </div>
+                    <div className="ds-portfolio-item">
+                        <div className="ds-portfolio-box-img">
+                            <img className="img-fluid" src={Portfolio2} alt="Realizacja-2"/>
+                        </div>
+                    </div>
+                </div>
+
+    );
+    export default PortfolioBox;
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PortfolioNav from './Portfolio-nav';
+import PortfolioBox from './Portfolio-box';
 
 const Portfolio = () => (
 
         <div className="ds-pportfolio-page">
             <div className="container">
                <PortfolioNav/>
+               <PortfolioBox/>
             </div>
         </div>
 
