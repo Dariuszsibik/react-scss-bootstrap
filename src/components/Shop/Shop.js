d5cb0045742c635517b82da98001e4f0261d9ef0
Dariusz Sibik <sibikdariusz@gmail.com>
2018-10-28 21:55:21 +0100

    add styles for Search

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,9 +7,9 @@ const Shop = () => (
                 <div className="container">
                     <div class="text-center ds-product-list-intro">
                         <span className="text-muted text-uppercase ds-text-small ds-page-intro-subheader">Lorem Ipsum dolor sit</span>
-                        <h2 class="text-uppercase ds-page-intro-header">Sklep<span className="text-primary">Online</span></h2>
+                        <h2 class="text-uppercase ds-page-intro-header">Sklep <span className="text-primary">Online</span></h2>
                         <div className="ds-hr ds-page-intro-rules"></div>
-                        <p className="ds-product-list-intro-desc">Magna laborum dolor laboris laborum. Nostrud anim ipsum aliqua officia anim commodo reprehenderit. Mollit velit laboris incididunt ullamco ex adipisicing in enim proident.</p>
+                        <p className="ds-product-list-intro-desc mb-5">Magna laborum dolor laboris laborum. Nostrud anim ipsum aliqua officia anim commodo reprehenderit. Mollit velit laboris incididunt ullamco ex adipisicing in enim proident.</p>
                     </div>
                 </div>
             </div>
