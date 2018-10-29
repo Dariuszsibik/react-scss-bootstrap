9ddba95afea85dd41cded0a1300940c3c80992b4
Dariusz Sibik <sibikdariusz@gmail.com>
2018-10-29 21:16:59 +0100

    styles for shop page

---
+++ src/components/Shop/Shop-paginacja.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Pagination = () => (
+
+                    <div className="text-center ds-pagination">
+                        <ul className="pagination justify-content-center">
+                            <li className="page-item disabled"><a href="/" className="page-link">&lt; poprzednia strona</a></li>
+                            <li className="page-item"><a href="/" className="page-link">następna strona &gt;</a></li>
+                        </ul>
+                    </div>
+
+    );
+    export default Pagination;
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Search from './Search.js';
 import ShopList from './Shop-list';
+import ShopPagination from './Shop-paginacja';
 
 const Shop = () => (
 
         <div className="ds-shop-page">
-            <div className="ds-product-list-intro">
                 <div className="container">
-                    <div class="text-center ds-product-list-intro">
+                    <div class="text-center mx-auto ds-product-list-intro">
                         <span className="text-muted text-uppercase ds-text-small ds-page-intro-subheader">Lorem Ipsum dolor sit</span>
                         <h2 class="text-uppercase ds-page-intro-header">Sklep <span className="text-primary">Online</span></h2>
                         <div className="ds-hr ds-page-intro-rules"></div>
                         <p className="ds-product-list-intro-desc mb-5">Magna laborum dolor laboris laborum. Nostrud anim ipsum aliqua officia anim commodo reprehenderit. Mollit velit laboris incididunt ullamco ex adipisicing in enim proident.</p>
                     </div>
                 </div>
-            </div>
         <Search/>
         <ShopList/>
+        <ShopPagination/>
         </div>
 
     );
